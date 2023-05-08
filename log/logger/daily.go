006b03ed764e5cfc842689fddb37c3de062dacab
肖轲 <xiaoke1754@stnts.com>
2023-05-08 13:59:28 +0800

    log

--- log/logger/daily.go
+++ log/logger/daily.go
@@ -2,14 +2,15 @@ package logger
 
 import (
 	"errors"
-	"gopkg.in/go-mixed/framework.v1/facades/config"
-	"path"
-	"strings"
-	"time"
-
 	rotatelogs "github.com/goravel/file-rotatelogs/v2"
 	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
+	"gopkg.in/go-mixed/framework.v1/facades/config"
+	"io"
+	"os"
+	"path"
+	"strings"
+	"time"
 
 	"gopkg.in/go-mixed/framework.v1/log/formatter"
 )
@@ -35,11 +36,11 @@ func (daily *Daily) Handle(channel string) (logrus.Hook, error) {
 	if err != nil {
 		return hook, errors.New("Config local file system for logger error: " + err.Error())
 	}
-
+	writer2 := os.Stdout
 	levels := getLevels(config.GetString(channel + ".level"))
 	writerMap := lfshook.WriterMap{}
 	for _, level := range levels {
-		writerMap[level] = writer
+		writerMap[level] = io.MultiWriter(writer, writer2)
 	}
 
 	return lfshook.NewHook(
